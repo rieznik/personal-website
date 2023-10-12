7b0fd0051054688da29452e27c690c9854509d9d
katerynarieznik <rieznik.k@gmail.com>
2023-10-12 17:24:48 +0300

    create grid template

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,8 @@
 	import { spring } from 'svelte/motion';
 	import { tweened } from 'svelte/motion';
 
+	import Tile from './Tile.svelte';
+
 	let coords = spring(
 		{ x: 50, y: 50 },
 		{
@@ -23,9 +25,34 @@
 		class="spotlight"
 		style="background: radial-gradient(circle at {$coords.x}px {$coords.y}px, #00000000 10px, #000000ee 250px);"
 	/>
+	<div class="container">
+		<div class="grid">
+			<Tile areaName="about" />
+			<Tile areaName="ukraine" />
+			<Tile areaName="notes" />
+			<Tile areaName="github" />
+			<Tile areaName="linkedin" />
+			<Tile areaName="mail" />
+			<Tile areaName="theme" />
+		</div>
+	</div>
 </div>
 
 <style>
+	.container {
+		max-width: 1200px;
+		margin: 0 auto;
+	}
+
+	.grid {
+		padding-top: 100px;
+		display: grid;
+		gap: 1rem;
+		grid-template-columns: repeat(4, 280px);
+		grid-template-rows: repeat(3, 280px);
+		grid-template-areas: 'about about ukraine notes' 'about about github notes' 'mail theme github linkedin';
+	}
+
 	.background {
 		height: 100vh;
 		width: 100vw;
---
+++ src/routes/Tile.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	export let areaName: String;
+</script>
+
+<div class="tile" style="grid-area: {areaName}" />
+
+<style>
+	.tile {
+		width: 100%;
+		height: 100%;
+		border-radius: 30px;
+		border: 1px solid rgba(255, 255, 255, 0.2);
+		background: rgba(0, 59, 124, 0.1);
+		backdrop-filter: blur(20px);
+	}
+</style>
